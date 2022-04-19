d816effa747b83ece9b128336d198af105a66f6e
Sebastian Marines <sebastian0marines@gmail.com>
2022-04-19 09:15:28 -0500

    Parse input and print AST

--- parsing.c
+++ parsing.c
@@ -6,6 +6,20 @@
 
 int main(int argc, char** argv) {
 
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Cheemscript = mpc_new("cheemscript");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+        " \
+         number : /-?[0-9]+/ ; \
+         operator : '+' | '-' | '*' | '/' ; \
+         expr : <number> | '(' <operator> <expr>+ ')' ; \
+         cheemscript : /^/ <operator> <expr>+ /$/ ; \
+        ",
+        Number, Operator, Expr, Cheemscript);
+
     puts("Cheemscript v0.0.1");
     puts("Press Ctrl+c to exit\n");
 
@@ -14,10 +28,18 @@ int main(int argc, char** argv) {
 
         add_history(input);
 
-        printf("No you're a %s\n", input);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Cheemscript, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
     }
 
+    mpc_cleanup(4, Number, Operator, Expr, Cheemscript);
     return 0;
 }
