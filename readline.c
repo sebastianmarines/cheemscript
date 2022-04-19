63a4f64bc170eb62e3ed7cc891af721252fa2da9
Sebastian Marines <sebastian0marines@gmail.com>
2022-04-19 09:01:16 -0500

    Move readline implementations to separate C file

---
+++ readline.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+void add_history(char* unused) {}
--- readline.h
+++ readline.h
@@ -4,18 +4,10 @@
 #ifdef _WIN32
 #include <string.h>
 
-static char buffer[2048];
 
-char* readline(char* prompt) {
-    fputs(prompt, stdout);
-    fgets(buffer, 2048, stdin);
-    char* cpy = malloc(strlen(buffer)+1);
-    strcpy(cpy, buffer);
-    cpy[strlen(cpy)-1] = '\0';
-    return cpy;
-}
+char* readline(char* prompt);
 
-void add_history(char* unused) {}
+void add_history(char* unused);
 
 #else
 #include <editline/readline.h>
