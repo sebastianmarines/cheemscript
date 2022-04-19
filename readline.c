2d5f3136ffd560a47106260002b5430c25ac8f9c
Sebastian Marines <sebastian0marines@gmail.com>
2022-04-19 14:17:18 -0500

    Fix readline.c preprocessor directives

--- readline.c
+++ readline.c
@@ -1,3 +1,4 @@
+#ifdef _WIN32
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,13 +6,15 @@
 
 static char buffer[2048];
 
-char* readline(char* prompt) {
+char *readline(char *prompt)
+{
     fputs(prompt, stdout);
     fgets(buffer, 2048, stdin);
-    char* cpy = malloc(strlen(buffer)+1);
+    char *cpy = malloc(strlen(buffer) + 1);
     strcpy(cpy, buffer);
-    cpy[strlen(cpy)-1] = '\0';
+    cpy[strlen(cpy) - 1] = '\0';
     return cpy;
 }
 
-void add_history(char* unused) {}
+void add_history(char *unused) {}
+#endif
